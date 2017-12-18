30c930cf783c8f9ed487833d9a39083ece2d8f44
Jon Adams <jadams@firetiger.net>
2017-12-18 14:56:30 -0500

    A little dependency injection pattern for main, and less trivial tests on void methods via stubbing

--- optimus prime.Tests/ProgramTest.cs
+++ optimus prime.Tests/ProgramTest.cs
@@ -1,14 +1,42 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Optimus.Prime.Tests {
     [TestFixture]
     public class ProgramTest {
+        // simple little action mock; due to Action<> and not having an interface for Program, easier to stub this than use mocking with Moq
+        private class ConsoleWriteActionStub {
+            public List<string> Messages { get; set; } = new List<string>(0);
+
+            public void Action(string value) {
+                Messages.Add(value);
+            }
+
+            public void ActionWithArgs(string value, object obj1, object obj2) {
+                Messages.Add(string.Format(value, obj1, obj2));
+            }
+        }
+
         [Test]
         public void TestMissingArgs() {
             Assert.DoesNotThrow(() => Program.Main(new string[0]));
             Assert.DoesNotThrow(() => Program.Main(new[] { "1" }));
         }
+
+        [Test]
+        public void TestMissingArgsSimple() {
+            var outputMock = new ConsoleWriteActionStub();
+
+            int first, second;
+            string third;
+            Assert.DoesNotThrow(() => Program.ParseArgs(new string[0], outputMock.Action, out first, out second, out third));
+            Assert.DoesNotThrow(() => Program.ParseArgs(new[] { "1" }, outputMock.Action, out first, out second, out third));
+            Assert.AreEqual(2, outputMock.Messages.Count, "Action call count did not match expected");
+            Assert.AreEqual("Must include two positive integers for an inclusive range to check for primes", outputMock.Messages.First());
+        }
+
 
         [Test]
         public void TestInvalidArgs() {
@@ -26,8 +54,17 @@ namespace Optimus.Prime.Tests {
         }
 
         [Test]
-        public void TestInverseArgs() {
+        public void TestInverseArgsSimple() {
             Assert.DoesNotThrow(() => Program.Main(new[] { "2", "1" }));
+        }
+
+        [Test]
+        public void TestInverseArgs() {
+            var outputMock = new ConsoleWriteActionStub();
+            Assert.DoesNotThrow(() => Program.Execute(outputMock.ActionWithArgs, 2, 1, ""));
+            Assert.AreEqual(2, outputMock.Messages.Count, "Action call count did not match expected");
+            Assert.IsTrue(outputMock.Messages[0].StartsWith("Results"), "Did not output any results");
+            Assert.IsTrue(outputMock.Messages[1].StartsWith("\nResults generated"), "Result timer output missing");
         }
 
         [Test]
--- optimus prime/Program.cs
+++ optimus prime/Program.cs
@@ -1,30 +1,43 @@
 using System;
 using System.Diagnostics;
-using System.Linq;
 using Optimus.Prime.Services;
 
 namespace Optimus.Prime {
     class Program {
         public static void Main(string[] args) {
+            int first, second;
+            string third;
+            ParseArgs(args, Console.WriteLine, out first, out second, out third);
+
+            Execute(Console.WriteLine, first, second, third);
+        }
+
+        internal static void ParseArgs(string[] args, Action<string> output, out int first, out int second, out string third) {
+            first = -1;
+            second = -1;
+            third = "";
+
             // `args` should accept two integers for a range; an optional third integer may select the generator to use
             if (args.Length < 2) {
-                Console.WriteLine("Must include two positive integers for an inclusive range to check for primes");
+                output("Must include two positive integers for an inclusive range to check for primes");
                 return;
             }
 
-            int first = -1;
-            int second = -1;
             if (!int.TryParse(args[0], out first) ||
                 !int.TryParse(args[1], out second)) {
-                Console.WriteLine("Must include two positive integers (32-bit max) for an inclusive range to check for primes");
+                output("Must include two positive integers (32-bit max) for an inclusive range to check for primes");
                 return;
             }
 
             // normalize algorithm argument, if available
-            var thirdArg = args.Length > 2 ? args[2].Trim().ToLowerInvariant() : null;
+            third = args.Length > 2 ? args[2].Trim().ToLowerInvariant() : null;
+        }
+
+        internal static void Execute(Action<string, object, object> output, int first, int second, string third) {
+            // should use an IoC container for picking the algorithms instead of using `new`, but skipping that step for simplicity of this demo
 
             var stopwatch = new Stopwatch();
-            if (thirdArg == "race") {
+            if (third == "race") {
                 // for fun, let's race algorithms:
                 var generator = new BasicPrimeNumberGenerator();
                 var generator2 = new SieveOfEratosthenesPrimeNumberGenerator();
@@ -35,12 +48,11 @@ namespace Optimus.Prime {
                 stopwatch2.Start();
                 var results2 = generator2.generate(first, second);
                 stopwatch2.Stop();
-                Console.WriteLine("\nBasicPrimeNumberGenerator               generated in {0}ms ({1} ticks)", stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks);
-                Console.WriteLine("SieveOfEratosthenesPrimeNumberGenerator generated in {0}ms ({1} ticks)", stopwatch2.ElapsedMilliseconds, stopwatch2.ElapsedTicks);
-
+                output("\nBasicPrimeNumberGenerator               generated in {0}ms ({1} ticks)", stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks);
+                output("SieveOfEratosthenesPrimeNumberGenerator generated in {0}ms ({1} ticks)", stopwatch2.ElapsedMilliseconds, stopwatch2.ElapsedTicks);
             } else {
                 // pick a generator
-                var generator = thirdArg == "alternate" || thirdArg == "a" || thirdArg == "eratosthenes"
+                var generator = third == "alternate" || third == "a" || third == "eratosthenes"
                                 ? (IPrimeNumberGenerator)new SieveOfEratosthenesPrimeNumberGenerator()
                                 : new BasicPrimeNumberGenerator();
 
@@ -49,8 +61,8 @@ namespace Optimus.Prime {
                 var results = generator.generate(first, second);
                 stopwatch.Stop();
 
-                Console.WriteLine(string.Join(", ", results));
-                Console.WriteLine("\nResults generated in {0}ms ({1} ticks)", stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks);
+                output("Results: {0}{1}", string.Join(", ", results), "");
+                output("\nResults generated in {0}ms ({1} ticks)", stopwatch.ElapsedMilliseconds, stopwatch.ElapsedTicks);
             }
         }
     }
